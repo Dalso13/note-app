00eee7b42605c34b497f57fc9939ced86a8afdf7
Dalso <flskwhdhk@gmail.com>
2024-01-11 16:54:09 +0900

    메인 스크린 디자인 대충 만들기

---
+++ lib/view/ui/main_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app/domain/model/note_model.dart';
+import 'package:note_app/view/view_model/main_view_model.dart';
+import 'package:provider/provider.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your note'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.list))
+        ],
+      ),
+      body: ListView(
+        children: viewModel.box.values.map((noteModel) {
+          return ListTile(
+            title: Text(noteModel.title),
+            subtitle: Text(noteModel.content),
+            tileColor: Color(noteModel.hexColor),
+            trailing: const Icon(Icons.delete),
+          );
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
